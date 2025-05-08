a73e9d6a95d09bf0c78a5568d5cf3265ee7bbf08
Vibish <vibishdevops@gmail.com>
2025-05-08 12:23:58 +0530

    Push

--- main.tf
+++ main.tf
@@ -1,32 +1,66 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "vibish-terraform-bucket" # Replace with your S3 bucket name
+    key            = "ecommerce-app/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock-table" # DynamoDB table for state locking
+  }
+}
+
+# Create DynamoDB table for state locking
+resource "aws_dynamodb_table" "terraform_lock_table" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "DevSecOps"
+  }
+}
+
+# Create EC2 instance for Tomcat server
 resource "aws_instance" "tomcat_server" {
   ami                         = "ami-0f1dcc636b69a6438"
   instance_type               = "t2.micro"
-  subnet_id                   = "subnet-0eb08298f65c9596d" # Replace with actual subnet ID for 'Main-VPC-subnet-public1-ap-south-1a'
-  vpc_security_group_ids      = ["sg-0c0339010561d889f"]
+  subnet_id                   = "subnet-0eb08298f65c9596d" # Replace with actual subnet ID
+  vpc_security_group_ids      = ["sg-0c0339010561d889f"]  # Replace with actual security group ID
   associate_public_ip_address = true
+  key_name                    = "terraform-keypair"
+
   tags = {
     Name = "TomcatServer"
+    Environment = "DevSecOps"
   }
-  key_name = "terraform-keypair"
+
+  # SSH connection configuration
   connection {
     type        = "ssh"
     host        = self.public_ip
     user        = "ec2-user"
     private_key = file("~/.ssh/terraform-keypair.pem") # Replace with the path to your private key
   }
+
+  # File provisioner to copy startup script
   provisioner "file" {
-    source      = "startup.sh" 
+    source      = "startup.sh"
     destination = "/tmp/startup.sh"
-    
   }
+
+  # Remote execution provisioner to run the startup script
   provisioner "remote-exec" {
     inline = [
       "sudo chmod +x /tmp/startup.sh",
       "sudo /tmp/startup.sh",
     ]
   }
-  
 }
